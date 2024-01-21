317f9862618789a124da01b8b729f999b0f36015
Sanidhya Khandelwal <sanidhiyakhandelwal450@gmail.com>
2024-01-21 18:40:30 +0530

    commit 2: understanding react flow & structure

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,13 @@
+import Chai from './chai';
+
+function App() {
+
+  return (
+    <>
+      <Chai />
+      <h1>this is h1</h1>
+    </>
+  )
+}
+
+export default App
---
+++ 01vitereact/src/chai.jsx
@@ -0,0 +1,9 @@
+function Chai() {
+    return (
+        <>
+            <h1>This is chai code in chai.jsx</h1>
+        </>
+    )
+}
+
+export default Chai;
