f1a4245951cd12e9f0e6d36289e907d455ed5f2d
Sanidhya Khandelwal <sanidhiyakhandelwal450@gmail.com>
2024-01-21 22:11:57 +0530

    useStateHook with assignment increa upto 5 & decrea upto 0

--- Hooks/src/App.jsx
+++ Hooks/src/App.jsx
@@ -1,30 +1,65 @@
+import { useState } from 'react'
 import './App.css'
 
-function App() {
+// function App() {
 
-  let count = 1;
-  const increament = () => {
-    count++;
-    console.log(count);
+//   let count = 1;
+//   const increament = () => {
+//     count++;
+//     console.log(count);
+//   }
+//   const decreament = () => {
+//     count--;
+//     console.log(count);
+//   }
+//   return (
+//     <>
+//       <h1>This is count {count}</h1>
+//       <button
+//         onClick={increament}
+//       >
+//         Increament counter
+//       </button>
+//       <br />
+//       <button
+//         onClick={decreament}
+//       >Decreament counter</button>
+//     </>
+//   )
+// }
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  function increament() {
+    if (count < 5)
+      setCount(count + 1);
+
   }
-  const decreament = () => {
-    count--;
-    console.log(count);
+  function decreament() {
+    if (count > 0)
+      setCount(count - 1);
   }
+
   return (
     <>
-      <h1>This is count {count}</h1>
+      <h1>This is useState Count {count}</h1>
       <button
         onClick={increament}
       >
         Increament counter
       </button>
+
       <br />
+
       <button
         onClick={decreament}
-      >Decreament counter</button>
+      >
+        Decreament counter
+      </button>
     </>
   )
 }
+
 
 export default App
