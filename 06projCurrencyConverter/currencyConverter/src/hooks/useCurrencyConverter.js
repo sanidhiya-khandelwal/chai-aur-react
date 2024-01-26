bb7ad104c4eea54e537a9356446056b66ad07a12
Sanidhya Khandelwal <sanidhiyakhandelwal450@gmail.com>
2024-01-26 19:50:16 +0530

    custom hooks done✅

---
+++ 06projCurrencyConverter/currencyConverter/src/hooks/useCurrencyConverter.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { useEffect, useState } from 'react'
+
+const useCurrencyConverter = (currency) => {
+
+    const [data, setData] = useState({});
+    useEffect(() => {
+        fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
+            .then(res => res.json)
+            .then(res => setData(res))
+        console.log(data);
+    }, [currency])
+
+    return data;
+}
+
+export default useCurrencyConverter
