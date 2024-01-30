b09a1de9c93fbb87767ce9e66556d8d656bfc158
Sanidhya Khandelwal <sanidhiyakhandelwal450@gmail.com>
2024-01-30 09:31:41 +0530

    Card,ThemeBtn components created and called in App.jsx

--- 09projThemeSwitcher/src/App.jsx
+++ 09projThemeSwitcher/src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import { ThemeProvider } from './contexts/theme'
 import { useEffect } from 'react';
 import './App.css'
+import ThemeBtn from './components/ThemeBtn';
+import Card from './components/Card';
 
 function App() {
   const [themeMode, setThemeMode] = useState('light');
@@ -31,7 +33,17 @@ function App() {
   return (
     // providing values to wrapped up components 
     <ThemeProvider value={{ themeMode, darkTheme, lightTheme }}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            <ThemeBtn />
+          </div>
 
+          <div className="w-full max-w-sm mx-auto">
+            <Card />
+          </div>
+        </div>
+      </div>
     </ThemeProvider>
   )
 }
