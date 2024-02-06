a861ba792c6fb3bc326fa067baf9c955ef597a58
Sanidhya Khandelwal <sanidhiyakhandelwal450@gmail.com>
2024-02-06 09:50:34 +0530

    App jsx ✅

--- 10todoContextLocal/src/App.jsx
+++ 10todoContextLocal/src/App.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 import { TodoProvider } from './contexts/TodoContext'
+import TodoForm from './components/TodoForm';
+import TodoItem from './components/TodoItem';
 
 function App() {
   // todos means there are many todo
@@ -95,9 +97,17 @@ function App() {
           <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
           <div className="mb-4">
             {/* Todo form goes here */}
+            <TodoForm />
           </div>
           <div className="flex flex-wrap gap-y-3">
             {/*Loop and Add TodoItem here */}
+            {
+              todos.map(todo => (
+                <div key={todo.id} className='w-full'>
+                  <TodoItem todo={todo} />
+                </div>
+              ))
+            }
           </div>
         </div>
       </div>
