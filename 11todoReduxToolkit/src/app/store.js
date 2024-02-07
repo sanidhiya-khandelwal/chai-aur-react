ae2e4998d759e344bdc0402f4a1b92f7e35d0227
Sanidhya Khandelwal <sanidhiyakhandelwal450@gmail.com>
2024-02-07 18:14:24 +0530

    created store partially

---
+++ 11todoReduxToolkit/src/app/store.js
@@ -0,0 +1,6 @@
+import { configureStore } from '@reduxjs/toolkit';
+
+/**
+ * store is a varibale with the help of which we are exporting configureStore() which takes an object mostly
+ */
+export const store = configureStore({})
