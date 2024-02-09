ea185694f69a477a256036fe49f06056e57f754e
Sanidhya Khandelwal <sanidhiyakhandelwal450@gmail.com>
2024-02-09 18:42:48 +0530

    components

--- 11todoReduxToolkit/src/App.jsx
+++ 11todoReduxToolkit/src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
+import AddTodo from './components/AddTodo'
+import Todo from './components/Todo'
 
 function App() {
 
   return (
     <>
-
+      <h1>Learn about redux toolkit</h1>
+      <AddTodo />
+      <Todo />
     </>
   )
 }
