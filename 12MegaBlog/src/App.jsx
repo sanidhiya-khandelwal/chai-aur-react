9cb99405b6bc6b259d76a5176c2bebe88dc7f9f6
Sanidhya Khandelwal <sanidhiyakhandelwal450@gmail.com>
2024-02-11 13:18:53 +0530

    step1 installed required dependencies

---
+++ 12MegaBlog/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  console.log(import.meta.env.VITE__APPWRITE_URL);
+  return (
+    <>
+      <h1>A blog app with appwrite</h1>
+    </>
+  )
+}
+
+export default App
