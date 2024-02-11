9f892b1ba95bf41ea160335afd9fab6bd40f99c6
Sanidhya Khandelwal <sanidhiyakhandelwal450@gmail.com>
2024-02-11 19:35:35 +0530

    step2 Handling env variables like the way they're handled in production

---
+++ 12MegaBlog/src/conf/conf.js
@@ -0,0 +1,13 @@
+/*
+    * creating a object and exporting it and we made sure whatever is returned
+    * is returned back is in the form of String and it can be a empty string  
+*/
+const conf = {
+    appwriteUrl: String(import.meta.env.VITE__APPWRITE_URL),
+    appwriteProjectId: String(import.meta.env.VITE__APPWRITE_PROJECT_ID),
+    appwriteDatabaseId: String(import.meta.env.VITE__APPWRITE_DATABASE_ID),
+    appwriteCollectionId: String(import.meta.env.VITE__APPWRITE_COLLECTION_ID),
+    appwriteBucketId: String(import.meta.env.VITE__APPWRITE_BUCKET_ID)
+}
+
+export default conf;
