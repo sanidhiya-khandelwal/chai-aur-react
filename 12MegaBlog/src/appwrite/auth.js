6ddc9b8f7c0196846fdb300c64833a9a626e9b84
Sanidhya Khandelwal <sanidhiyakhandelwal450@gmail.com>
2024-02-12 19:05:54 +0530

    comments updated

--- 12MegaBlog/src/appwrite/auth.js
+++ 12MegaBlog/src/appwrite/auth.js
@@ -5,21 +5,21 @@ import { Client, Account, ID } from "appwrite"; //step 2 copy this line from doc
 /**
  * we could have copy pasted the whole code from documentation as it is in this file
  * but we did not did that. Reason-> ui code and bizz logic sb ek jagah is not good.
- * So to improve code quality we will export class and object
+ * So to improve code quality we will export object
  */
 
 /*
  * step 3  
  * exporting object instead of class why coz jo bhi usey use krega usey object bnana
  * padega iss class se tbhi toh saar methods use kr paenge so better way is to create
- *  an object and export it as objects me saare methds honge
+ *  an object and export it as object me saare methds ki access honge like login,logout etc
 */
 //create class
 export class AuthService {
     // step 3.1 create properties
     client = new Client(); //client bnta h "new Client()" krkr
     account; //right now "account" is a variable and we did not do"=new Account(client);" reason "client" abhi bna nhi h as client needs ".setEndpoint()" and ".setProject()" and we've not created them above reason is there is no senese in doing that as y class by default bn jaenge that is wastage of resource ... what we want ki jb koi object bnae "authService" tb client bne and then account ka access ho so we will now use constructor
-
+    // step 3.2 create constructor
     constructor() {
         this.client  //"client" ka reference using "this" and setting ".setEndpoint" and ".setProject"
             .setEndpoint(conf.appwriteUrl)
@@ -29,6 +29,7 @@ export class AuthService {
         this.account = new Account(this.client);
     }
 
+    // step 3.3 methods
     //FUNCTIONALITY TO CREATE ACCOUNT
     /* 
         * we use async await instead of promises reason being we want ki jb tk account create na ho ..cheeze aage na bdhe
@@ -96,7 +97,7 @@ export default authService
 /**
  * u can access any method or property using example "authService.logout"
  * Now since we crated this file and if we change tje service from AppWrite to Firebase then
- * we need to chnage in this files functions only like chnage only inside logout()
+ * we need to chnage in this files functions only like chnage only inside getCurrentUser(),logout(),login(),createUser(), function
  * 
  */
 
